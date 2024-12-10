2f33c020cea1ba3811f6ae01a15abd63e5a8c778
Sergey Abramov <s.abramov@glowbyte.io>
2024-12-10 12:36:58 +0300

    day10

--- src/day10.rs
+++ src/day10.rs
@@ -1,1 +1,124 @@
+use std::collections::HashSet;
 
+use crate::utils::{read_lines, try_move, Direction, Location};
+
+type TrailMap = Vec<Vec<usize>>;
+pub fn read_map(filename: &str) -> anyhow::Result<TrailMap> {
+    Ok(read_lines(filename)?
+        .map_while(|line| line.ok())
+        .map(|line| {
+            line.chars()
+                .map_while(|c| c.to_digit(10).map(|c| c as usize))
+                .collect()
+        })
+        .collect())
+}
+
+fn try_step(loc: Location, map: &TrailMap, dir: Direction) -> Option<Location> {
+    let new_loc = try_move(loc, dir)?;
+
+    if new_loc.0 >= map.len()
+        || new_loc.1 >= map[new_loc.0].len()
+        || map[loc.0][loc.1] + 1 != map[new_loc.0][new_loc.1]
+    {
+        None
+    } else {
+        Some(new_loc)
+    }
+}
+
+pub fn add_trail_peaks(start: Location, map: &TrailMap, set: &mut HashSet<Location>) {
+    if map[start.0][start.1] == 9 {
+        set.insert(start);
+    } else {
+        [(0, 1), (0, -1), (1, 0), (-1, 0)]
+            .into_iter()
+            .for_each(|dir| {
+                if let Some(loc) = try_step(start, map, dir) {
+                    add_trail_peaks(loc, map, set);
+                };
+            });
+    }
+}
+
+pub fn count_trails(start: Location, map: &TrailMap) -> usize {
+    if map[start.0][start.1] == 9 {
+        1
+    } else {
+        [(0, 1), (0, -1), (1, 0), (-1, 0)]
+            .into_iter()
+            .map(|dir| {
+                if let Some(loc) = try_step(start, map, dir) {
+                    count_trails(loc, map)
+                } else {
+                    0
+                }
+            })
+            .sum()
+    }
+}
+#[cfg(test)]
+mod test {
+    use crate::day10::count_trails;
+
+    use super::Location;
+    use super::{add_trail_peaks, read_map, try_step};
+    use std::collections::HashSet;
+
+    #[test]
+    fn test_step() {
+        let loc = (0, 0);
+        let map = vec![vec![1, 2], vec![3, 4]];
+
+        assert!(matches!(try_step(loc, &map, (1, 0)), None));
+        assert!(matches!(try_step(loc, &map, (0, 1)), Some((0, 1))));
+        assert!(matches!(try_step(loc, &map, (-1, 0)), None));
+        assert!(matches!(try_step(loc, &map, (0, -1)), None));
+    }
+
+    #[test]
+    fn hoof_it() {
+        let input = read_map("data/10.in").expect("Couldn't read file");
+        let value = (&input)
+            .iter()
+            .enumerate()
+            .map(|(row, vec)| {
+                vec.iter()
+                    .enumerate()
+                    .map(|(col, &val)| {
+                        if val == 0 {
+                            let mut peaks: HashSet<Location> = HashSet::new();
+                            add_trail_peaks((row, col), &input, &mut peaks);
+                            peaks.len()
+                        } else {
+                            0
+                        }
+                    })
+                    .sum::<usize>()
+            })
+            .sum::<usize>();
+
+        assert_eq!(value, 538);
+    }
+
+    #[test]
+    fn hoof_it_rating() {
+        let input = read_map("data/10.in").expect("Couldn't read file");
+        let value = (&input)
+            .iter()
+            .enumerate()
+            .map(|(row, vec)| {
+                vec.iter()
+                    .enumerate()
+                    .map(|(col, &val)| {
+                        (val == 0)
+                            .then_some(count_trails((row, col), &input))
+                            .unwrap_or(0)
+                    })
+                    .sum::<usize>()
+            })
+            .sum::<usize>();
+
+        assert_eq!(value, 1110);
+    }
+}
--- src/utils.rs
+++ src/utils.rs
@@ -2,6 +2,9 @@ use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 
+pub type Location = (usize, usize);
+pub type Direction = (isize, isize);
+
 // The output is wrapped in a Result to allow matching on errors.
 // Returns an Iterator to the Reader of the lines of the file.
 pub fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
@@ -10,4 +13,11 @@ where
 {
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
+}
+
+pub fn try_move(loc: Location, dir: Direction) -> Option<Location> {
+    Some((
+        loc.0.checked_add_signed(dir.0)?,
+        loc.1.checked_add_signed(dir.1)?,
+    ))
 }
